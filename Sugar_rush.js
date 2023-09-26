8d0d55609a99df52210c3ad1d6d87c043c08c066
mariapdp <144926252+mariapdp@users.noreply.github.com>
2023-09-26 10:17:51 -0300

    Update Sugar_rush.js

--- Sugar_rush.js
+++ Sugar_rush.js
@@ -1,1 +1,25 @@
+  function abrirPagina(buttonId) {
+    let pagina;
 
+    switch (buttonId) {
+        case 'btnSobreNos':
+            pagina = 'sobre_nos.html';
+            break;
+        case 'btnMenu':
+            pagina = 'menu.html';
+            break;
+        case 'btnContato':
+            pagina = 'contato.html';
+            break;
+        case 'btnHome':
+            pagina = '';
+            break;
+        case 'btnsaibaMais':
+            pagina ='';
+            break;
+        default:
+            return; 
+    }
+
+    window.location.href = erro.html;
+}
